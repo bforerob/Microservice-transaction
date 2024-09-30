d396e6c1f3828b9a0c80af7eaf82898fcbd51cba
bforerob <bforerob@unal.edu.co>
2024-09-30 10:36:53 -0500

    Repo init

---
+++ src/test/java/com/microservice_transaction/MicroserviceTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
